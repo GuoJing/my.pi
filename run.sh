0e0c4c42f52e172ff79818ba5ce853c74440d5f9
GuoJing <soundbbg@gmail.com>
2014-05-17 10:29:33 +0800

    init commit

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PIDFILE=/tmp/web.pid
+
+case $1 in
+   start)
+      echo $$ > ${PIDFILE};
+      source /home/work/venv/bin/activate
+      exec python /home/work/web/web.py >/dev/null
+   ;;
+   stop)
+      kill `cat ${PIDFILE}` && rm ${PIDFILE};;
+   *)
+      echo "usage: scraper {start|stop}" ;;
+esac
+exit 0
